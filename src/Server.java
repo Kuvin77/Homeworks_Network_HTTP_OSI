c7d19f04c0c0ca8b81a46eb3c3ee4edf23054cb7
Kuvin77 <v.kustyshev@gmail.com>
2022-05-18 16:35:25 +0300

    First commit

---
+++ src/Server.java
@@ -0,0 +1,26 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+
+        int port = 8089;
+
+        try (ServerSocket serverSocket = new ServerSocket(port);
+             Socket clientSocket = serverSocket.accept();
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+
+            String resp = in.readLine();
+            out.println(String.format("Привет, %s, твой порт %d", resp, clientSocket.getPort()));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
